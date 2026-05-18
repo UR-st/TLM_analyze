dbbef54d1dea2b2c3169d9342415517e2d329346
UR-st <usui.176hddry@outlook.jp>
2026-05-18 15:16:44 +0900

    a

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import numpy as np
 import func_tle as fn_tle
 from class_def import TLE_Elements_,TLE_Hex_
-TLE_HEX = TLE_Hex_
-TLE_HEX = fn_tle.tle_2_MRAM("tle.txt","TLE_cmd")
+import configparser
+import os
 
-print(vars(TLE_HEX))
+# 設定ファイルのパス
+current_dir = os.path.dirname(__file__)
+config_path = os.path.join(current_dir, "config.ini")
+
+# INIファイルの読み込み
+config = configparser.ConfigParser()
+config.read(config_path, encoding="utf-8")
+#TLE変更scriptの作成
+if config.getboolean("RUN_SETTINGS", "TLE_MRAM"):
+    TLE_HEX = TLE_Hex_
+    TLE_HEX = fn_tle.tle_2_MRAM("tle.txt","TLE_cmd")
+
 
 
